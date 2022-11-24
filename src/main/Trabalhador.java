22ce11c5bab6227bd998fc2d9c523c763876b0e0
Júlia <julia.valadares99@gmail.com>
2022-11-24 11:20:14 -0300

    feat: atualiza adapter para Trabalhador e cria classe trabalhador

--- src/main/Trabalhador.java
+++ src/main/Trabalhador.java
@@ -1,5 +1,21 @@
 package main;
 
 public class Trabalhador {
+    IRegimeTrabalho regimeTrabalho;
+    TrabalhadorAdapter persistencia;
+
+    public Trabalhador() {
+        regimeTrabalho = new PessoaJuridica();
+        persistencia = new TrabalhadorAdapter(regimeTrabalho);
+    }
+
+    public void setSalario(float salario) {
+        regimeTrabalho.setSalario(salario);
+        persistencia.atualizarSalario();
+    }
+
+    public float getregimeTrabalho() {
+        return persistencia.buscaSalario();
+    }
     
 }
